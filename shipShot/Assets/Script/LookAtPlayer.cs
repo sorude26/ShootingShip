dadc58c046e1b29f6989dc2f0cef62e615905b8f
Chi-ni <dokoitsu0506@gmail.com>
2021-07-26 13:06:07 +0900

    敵をプレイヤーに向かせるスクリプトとスコアを表示するスクリプトを追加

---
+++ shipShot/Assets/Script/LookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    GameObject playerObj;
+    Vector3 toPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObj = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        toPlayer = (playerObj.transform.position - this.transform.position).normalized;
+        this.transform.up = toPlayer;
+    }
+}
