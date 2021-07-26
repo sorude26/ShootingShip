f972b53db8b80e5487936eb4be298cbb94ee3932
Chi-ni <dokoitsu0506@gmail.com>
2021-07-26 13:46:29 +0900

    Merge branch 'main' of github.com:sorude26/shootingShip

--- shipShot/Assets/Script/ShipControl.cs
+++ shipShot/Assets/Script/ShipControl.cs
@@ -10,7 +10,7 @@ public class ShipControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Rb = GetComponent<Rigidbody2D>(); 
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
