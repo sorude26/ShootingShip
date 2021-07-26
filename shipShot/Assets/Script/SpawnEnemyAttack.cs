10abcfec21a80923629d0f67eebb8ff13489b5af
Chi-ni <dokoitsu0506@gmail.com>
2021-07-26 17:12:36 +0900

    敵の砲弾を生成するスクリプトを作成

---
+++ shipShot/Assets/Script/SpawnEnemyAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyAttack : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemyCannon;
+    [SerializeField]
+    float shotInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Shot());
+    }
+
+    IEnumerator Shot()
+    {
+        yield return new WaitForSeconds(shotInterval);
+
+        Instantiate(enemyCannon).transform.position = this.transform.position;
+    }
+}
